f841f0f3975625466b0cb94dc66101bb63b08e92
Mrnaveen00 <95097145+Mrnaveen00@users.noreply.github.com>
2022-08-24 18:15:21 +0530

    Again_adding img

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import styles from '../styles/Help.module.css'
-
+import img1 from './h/help.webp'
 function Help() {
     return (
         <div>
             <Navbar />
 <p className={styles.test}>testing</p>
             Help
-
+<div className={styles.Help}>
+<img src={img1}/>;
+</div>
+<div className={styles.Helptext}>
+    Browse Our Topics
+</div>
 
 
 
